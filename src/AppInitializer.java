e14a0e510afb5d1ab14d909fc929da9e5b7fd3a2
iumayanga <pl.isuru97@gmail.com>
2022-06-08 15:47:39 +0530

    Rename and modify the NewClass.java file

---
+++ src/AppInitializer.java
@@ -0,0 +1,9 @@
+public class AppInitializer {
+    public static void main(String[] args) {
+        Operations operations = new Operations();
+        operations.add(1, 2);
+        operations.sub(1, 2);
+        operations.mul(1, 2);
+        operations.div(1, 2);
+    }
+}
