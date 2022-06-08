7e6edfcf84bafd6dd3ddef41a49a37763a57e0b7
iumayanga <pl.isuru97@gmail.com>
2022-06-08 19:25:52 +0530

    Modify the AppInitializer.java file

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -3,9 +3,9 @@ import Cal.Operations;
 public class AppInitializer {
     public static void main(String[] args) {
         Operations operations = new Operations();
-        operations.add(1, 2);
-        operations.sub(1, 2);
-        operations.mul(1, 2);
-        operations.div(1, 2);
+        int add = operations.add(1, 2);
+        int sub = operations.sub(1, 2);
+        int mul = operations.mul(1, 2);
+        double div = operations.div(1, 2);
     }
 }
